8f3633e7a4e0727df051da8c71268e71baf58bc8
abdulalh-al-mohammad <abdullahalmuhammad6@mail.com>
2024-02-13 15:35:59 +0600

    add click handle and color on button

--- js/tube.js
+++ js/tube.js
@@ -10,17 +10,25 @@ const btnContainer = document.getElementById('btn-container');
 const displayButtonList = (buttonList) =>{
     buttonList.forEach(button => {
         const btn = document.createElement('Button');
-        btn.classList.add('btn', 'btn-success', 'me-3', 'px-4');
+        btn.classList.add('btn', 'btn-secondary', 'me-3', 'px-4');
         btn.innerText= button.category;
         btn.id= button.category_id;
+        if(btn.id === '1000'){
+            btn.classList.add('btn-danger');
+        }
         btnContainer.appendChild(btn) 
     });
-    btnContainer.addEventListener('click', (e) =>{
+}
+btnContainer.addEventListener('click', (e) =>{
+    if(e.target.id !== 'btn-container'){
+        for (const btn of btnContainer.children) {
+            btn.classList.remove('btn-danger')
+        }
         e.target.classList.add('btn-danger');
         loadData(e.target.id)
-        console.log(e.target.id);        
-    })
-}
+        console.log(e.target);
+    }   
+})
 const loadData = async(id) =>{
     const user = `https://openapi.programming-hero.com/api/videos/category/${id}`;
     const res = await fetch(user);
@@ -36,12 +44,12 @@ const displayData = (users) =>{
         dataDiv.classList.add('col')
         dataDiv.innerHTML= `
         <div class="card h-100">
-            <img src="${user.thumbnail}" class="card-img-top" alt="...">
+            <img src="${user.thumbnail}" class="card-img-top h-50 object-fit-cover" alt="...">
             <div class="card-body d-flex">
                 <img src="${user.authors[0].profile_picture}" class="rounded-circle me-3 profile_picture" alt="...">
                 <div>
                 <h6 class="card-title fw-semibold">${user.title}</h6>
-                <p class="card-text">${user.authors[0].profile_name}</p>
+                <p class="card-text">${user.authors[0].profile_name} <span class= "ms-2">${user.authors[0].verified  ? `<img src= "../images/Group 3.png"></span>` : ''}</span> </p>
                 <p class="card-text">${user.others.views} views</p>
                 </div>
                 </div>
@@ -52,4 +60,3 @@ const displayData = (users) =>{
         }
         
         loadData(1000)
-        // <p class="card-text">${user.authors[0].verified? verified : ''}</p>
