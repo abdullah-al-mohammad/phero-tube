073e0215c1b94a96aef1e95ca6660baae47a2736
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2024-02-14 09:08:01 +0600

    set no data found is data empty and img height

--- js/tube.js
+++ js/tube.js
@@ -6,6 +6,7 @@
     // console.log(data);
 
 })();
+
 const btnContainer = document.getElementById('btn-container');
 const displayButtonList = (buttonList) =>{
     buttonList.forEach(button => {
@@ -16,47 +17,67 @@ const displayButtonList = (buttonList) =>{
         if(btn.id === '1000'){
             btn.classList.add('btn-danger');
         }
-        btnContainer.appendChild(btn) 
+        btnContainer.appendChild(btn);
     });
 }
+// event listener
 btnContainer.addEventListener('click', (e) =>{
+    toggoleSpinner(true)
+    if(e.target.data === '0'){
+        dataContainer.innerHTML= `<p> no data found </p>`
+    }
     if(e.target.id !== 'btn-container'){
         for (const btn of btnContainer.children) {
-            btn.classList.remove('btn-danger')
+            btn.classList.remove('btn-danger');
         }
         e.target.classList.add('btn-danger');
         loadData(e.target.id)
-        console.log(e.target);
-    }   
+    }
+    toggoleSpinner(false)
 })
+// loading spinner
+const toggoleSpinner = (isLoading)=>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('d-none')
+    }else{
+        loadingSpinner.classList.add('d-none')
+    }
+    console.log(loadingSpinner);
+}
+
+const dataContainer = document.getElementById('data-container')
+toggoleSpinner(true)
 const loadData = async(id) =>{
     const user = `https://openapi.programming-hero.com/api/videos/category/${id}`;
     const res = await fetch(user);
     const data = await res.json();
     displayData(data.data);  
 }
+
 const displayData = (users) =>{
-    const dataContainer = document.getElementById('data-container')
     dataContainer.innerHTML= '';
-    users.forEach(user =>{
-        // console.log(user.others);
+    if (users.length === 0) { // If no data is found
+        dataContainer.innerHTML = `<h5 class= "position-absolute top-50 start-50"> no data found ...</h5>`;
+    }
+    users.forEach(data =>{
         const dataDiv = document.createElement('div');
         dataDiv.classList.add('col')
         dataDiv.innerHTML= `
         <div class="card h-100">
-            <img src="${user.thumbnail}" class="card-img-top h-50 object-fit-cover" alt="...">
+            <img src="${data.thumbnail}" class="card-img-top h-50 object-fit-cover" alt="...">
             <div class="card-body d-flex">
-                <img src="${user.authors[0].profile_picture}" class="rounded-circle me-3 profile_picture" alt="...">
+                <img src="${data.authors[0].profile_picture}" class="rounded-circle me-3 profile_picture" alt="...">
                 <div>
-                <h6 class="card-title fw-semibold">${user.title}</h6>
-                <p class="card-text">${user.authors[0].profile_name} <span class= "ms-2">${user.authors[0].verified  ? `<img src= "../images/Group 3.png"></span>` : ''}</span> </p>
-                <p class="card-text">${user.others.views} views</p>
+                <h6 class="card-title fw-semibold">${data.title}</h6>
+                <p class="card-text">${data.authors[0].profile_name} <span class= "ms-2">${data.authors[0].verified  ? `<img src= "../images/Group 3.png"></span>` : ''}</span> </p>
+                <p class="card-text">${data.others.views} views</p>
                 </div>
                 </div>
                 </div>
                 `
                 dataContainer.appendChild(dataDiv)
             })
+            toggoleSpinner(false)
         }
-        
         loadData(1000)
